82c098b1c52a65c50a2ab6a9a2089a8764332def
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-23 20:37:28 -0600

    first model implemented

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/shared/Home';
 import About from './components/shared/About';
 import NoMatch from './components/shared/NoMatch';
+import Navbar from './components/shared/Navbar';
+import Blogs from './components/blogs/Blogs';
+import Blog from './components/blogs/Blog';
 // in charge of nav routes 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/blogs" component={Blogs} />
+        <Route exact path="/blogs/:id" component={Blog} />
         <Route component={NoMatch} />
       </Switch>
     </>
