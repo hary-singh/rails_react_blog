4971fce059e9cd1b1edcc25a2044010c57816370
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-23 19:39:27 -0600

    front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/shared/Home';
+import About from './components/shared/About';
+import NoMatch from './components/shared/NoMatch';
+// in charge of nav routes 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route component={NoMatch} />
+      </Switch>
+    </>
+  )
 }
-
 export default App;
